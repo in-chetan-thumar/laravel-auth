e9f32a8d9054b1207f8c6630731f2344340cb97d
tiez-hardeep <hardeep.pandya@tiez.nl>
2017-11-27 19:28:23 +0530

    Custom Laravel Auth

---
+++ src/LaravelAuthServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace InChetanThumar\LaravelAuth;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelAuthServiceProvider extends ServiceProvider {
+    
+    protected $defer = true;
+    
+    public function boot()
+    {
+        
+    }
+    
+    public function register()
+    {
+        
+    }
+}
